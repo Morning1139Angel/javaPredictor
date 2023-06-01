ebd99e6a4a1a551e373c4a4a590797e87cc2d95e
amir khazama <amirkhazama1139@gmail.com>
2023-06-01 16:36:59 +0330

    gag

--- Predictor/src/main/java/hardwar/branch/prediction/judged/GAg/GAg.java
+++ Predictor/src/main/java/hardwar/branch/prediction/judged/GAg/GAg.java
@@ -1,82 +1,47 @@
 package hardwar.branch.prediction.judged.GAg;
-
-import hardwar.branch.prediction.shared.*;
-import hardwar.branch.prediction.shared.devices.*;
-
+import hardwar.branch.prediction.shared.*;import hardwar.branch.prediction.shared.devices.*;
 import java.util.Arrays;
-
 public class GAg implements BranchPredictor {
     private final ShiftRegister BHR; // branch history register
     private final Cache<Bit[], Bit[]> PHT; // page history table
     private final ShiftRegister SC; // saturated counter register
-
-    public GAg() {
-        this(4, 2);
+    public GAg() {        this(4, 2);
     }
-
-    /**
-     * Creates a new GAg predictor with the given BHR register size and initializes the BHR and PHT.
-     *
-     * @param BHRSize the size of the BHR register
-     * @param SCSize  the size of the register which hold the saturating counter value and the cache block size
-     */
-    public GAg(int BHRSize, int SCSize) {
-        // TODO : complete the constructor
+    /**     * Creates a new GAg predictor with the given BHR register size and initializes the BHR and PHT.
+     *     * @param BHRSize the size of the BHR register
+     * @param SCSize  the size of the register which hold the saturating counter value and the cache block size     */
+    public GAg(int BHRSize, int SCSize) {        // TODO : complete the constructor
         // Initialize the BHR register with the given size and no default value
-        this.BHR = new SIPORegister("bhr", BHRSize );
-
+        this.BHR = new SIPORegister("bhr", BHRSize, null );
         // Initialize the PHT with a size of 2^size and each entry having a saturating counter of size "SCSize"
-        this.PHT = new PageHistoryTable(pow(2,BHRSize), SCSize);
-
+        this.PHT = new PageHistoryTable(2^BHRSize, SCSize);
         // Initialize the SC register
-        this.SC =  new SIPORegister("sc", SCSize );
+        this.SC =  new SIPORegister("sc", SCSize ,null);
     }
-
-    /**
-     * Predicts the result of a branch instruction based on the global branch history
-     *
-     * @param branchInstruction the branch instruction
-     * @return the predicted outcome of the branch instruction (taken or not taken)
-     */
-    @Override
-    public BranchResult predict(BranchInstruction branchInstruction) {
-        Bit[] bhrValue = this.BHR.read();
-        Bit[] readBlock = this.PHT.get(bhrValue);
-        this.SC.load(readBlock);
-        return BranchResult.of(readBlock[0]);
-
+    /**     * Predicts the result of a branch instruction based on the global branch history
+     *     * @param branchInstruction the branch instruction
+     * @return the predicted outcome of the branch instruction (taken or not taken)     */
+    @Override    public BranchResult predict(BranchInstruction branchInstruction) {
+        Bit[] bhrValue = this.BHR.read();        Bit[] readBlock = this.PHT.get(bhrValue);
+        this.SC.load(readBlock);        return BranchResult.of(readBlock[0].getValue());
     }
-
     /**
-     * Updates the values in the cache based on the actual branch result
-     *
-     * @param instruction the branch instruction
-     * @param actual      the actual result of the branch condition
-     */
-    @Override
+     * Updates the values in the cache based on the actual branch result     *
+     * @param instruction the branch instruction     * @param actual      the actual result of the branch condition
+     */    @Override
     public void update(BranchInstruction instruction, BranchResult actual) {
-        
-        if (actual.isTaken){
-            this.SC = CombinationalLogic.saturateCount(this.SC, up);
-        } else {
-            this.SC = CombinationalLogic.saturateCount(this.SC, down);
-        }
-
-        this.BHR.insert(BranchResult.of(actual)));
+        if (BranchResult.isTaken(actual)){            this.SC.load(CombinationalLogic.count(this.SC.read(), true,CountMode.SATURATING));
+        }        else {
+            this.SC.load(CombinationalLogic.count(this.SC.read(), false,CountMode.SATURATING));        }
+        this.BHR.insert(Bit.of(BranchResult.isTaken(actual)));
     }
-
-
     /**
-     * @return a zero series of bits as default value of cache block
-     */
-    private Bit[] getDefaultBlock() {
-        Bit[] defaultBlock = new Bit[SC.getLength()];
-        Arrays.fill(defaultBlock, Bit.ZERO);
-        return defaultBlock;
+     * @return a zero series of bits as default value of cache block     */
+    private Bit[] getDefaultBlock() {        Bit[] defaultBlock = new Bit[SC.getLength()];
+        Arrays.fill(defaultBlock, Bit.ZERO);        return defaultBlock;
     }
-
-    @Override
-    public String monitor() {
-        return "GAg predictor snapshot: \n" + BHR.monitor() + SC.monitor() + PHT.monitor();
-    }
+    @Override    public String monitor() {
+        return "GAg predictor snapshot: \n" + BHR.monitor() + SC.monitor() + PHT.monitor();    }
+    public static void main(String[] args) {
+        GAg gag = new GAg(4, 2);    }
 }
